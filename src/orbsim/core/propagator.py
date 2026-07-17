1edb61ba979342cf6d45a8e6673cf8ec88ce54ac
PanSoulas <soulaspan281@gmail.com>
2026-07-17 18:38:15 +0300

    feat: implement datetime_to_jd and delta_minutes_from_epoch in propagator.py

---
+++ src/orbsim/core/propagator.py
@@ -0,0 +1,9 @@
+from orbsim._core import TLEElements
+from datetime import datetime
+
+def datetime_to_jd(dt: datetime) -> float:
+    Julian_date = 367 * dt.year - int(7 * (dt.year + int(dt.month + 9) // 12) // 4) + int(275 * dt.month // 9) + dt.day + 1721013.5 + (dt.hour + dt.minute / 60 + dt.second / 3600) / 24
+    return Julian_date
+
+def delta_minutes_from_epoch(date_time: datetime, epoch: TLEElements) -> float:
+    return (datetime_to_jd(date_time) - epoch.epoch_jd) * 1440
